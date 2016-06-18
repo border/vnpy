2858b08ea34f94098ed5299852712daef7da10bd
Bian Jiang <borderj@gmail.com>
2016-06-18 16:43:04 +0800

    add ctaAlgo/strategyAtrRsi.py

--- vn.trader/ctaAlgo/strategyAtrRsi.py
+++ vn.trader/ctaAlgo/strategyAtrRsi.py
@@ -1,9 +1,9 @@
 # encoding: UTF-8
 
 """
-1、周期15分钟
-2、均线采取EMA,10周期和20周期交叉决定多空，永久在市。
-3、过滤器采用前高前低，即，多单开仓必须符合价格比前一个EMA10上最高价高，反之亦然。
+一个ATR-RSI指标结合的交易策略，适合用在股指的1分钟和5分钟线上。
+
+（作者不对交易盈利做任何保证，策略代码仅供参考。）
 """
 
 
@@ -15,69 +15,76 @@ import numpy as np
 
 
 ########################################################################
-class DoubleEmaStrategy(CtaTemplate):
+class AtrRsiStrategy(CtaTemplate):
     """结合ATR和RSI指标的一个分钟线交易策略"""
-    className = 'DoubleEmaStrategy'
-    author = u'Bian Jiang'
-
-    # 仓位
-    pre_pos = 20000
-
-    orderDict = {}          # vtSymbol.orderType:orderID 保存委托代码的字典
+    className = 'AtrRsiStrategy'
+    author = u'用Python的交易员'
 
     # 策略参数
+    atrLength = 22          # 计算ATR指标的窗口数   
+    atrMaLength = 10        # 计算ATR均线的窗口数
+    rsiLength = 5           # 计算RSI的窗口数
+    rsiEntry = 16           # RSI的开仓信号
+    trailingPercent = 0.8   # 百分比移动止损
     initDays = 10           # 初始化数据所用的天数
-
-    fastEmaLength = 5      # 10 EMA
-    slowEmaLength = 10      # 20 EMA
 
     # 策略变量
     bar = None                  # K线对象
     barMinute = EMPTY_STRING    # K线当前的分钟
 
-    bufferSize = 10                    # 需要缓存的数据的大小
+    bufferSize = 100                    # 需要缓存的数据的大小
     bufferCount = 0                     # 目前已经缓存了的数据的计数
-
     highArray = np.zeros(bufferSize)    # K线最高价的数组
     lowArray = np.zeros(bufferSize)     # K线最低价的数组
     closeArray = np.zeros(bufferSize)   # K线收盘价的数组
+    
+    atrCount = 0                        # 目前已经缓存了的ATR的计数
+    atrArray = np.zeros(bufferSize)     # ATR指标的数组
+    atrValue = 0                        # 最新的ATR指标数值
+    atrMa = 0                           # ATR移动平均的数值
 
-    fastEmaArray = np.zeros(bufferSize) # fast EMA 指标的数组
-    slowEmaArray = np.zeros(bufferSize) # fast EMA 指标的数组
+    rsiValue = 0                        # RSI指标的数值
+    rsiBuy = 0                          # RSI买开阈值
+    rsiSell = 0                         # RSI卖开阈值
+    intraTradeHigh = 0                  # 移动止损用的持仓期内最高价
+    intraTradeLow = 0                   # 移动止损用的持仓期内最低价
 
-    fastEmaValue = 0                    # 最新 fast EMA指标数值
-    slowEmaValue = 0                    # 最新 slow EMA指标数值
-
-    fastEmaTradeHigh = 0                    # EMA10上最高价高
-    fastEmaTradeLow = 0                     # EMA10上最高价低
+    orderList = []                      # 保存委托代码的列表
 
     # 参数列表，保存了参数的名称
     paramList = ['name',
                  'className',
                  'author',
                  'vtSymbol',
-                 'fastEmaLength',
-                 'slowEmaLength']
+                 'atrLength',
+                 'atrMaLength',
+                 'rsiLength',
+                 'rsiEntry',
+                 'trailingPercent']    
 
     # 变量列表，保存了变量的名称
     varList = ['inited',
                'trading',
                'pos',
-               'fastEmaValue',
-               'slowEmaValue',
-               'fastEmaTradeHigh',
-               'fastEmaTradeLow',
-               'bufferCount']
+               'atrValue',
+               'atrMa',
+               'rsiValue',
+               'rsiBuy',
+               'rsiSell']  
 
     #----------------------------------------------------------------------
     def __init__(self, ctaEngine, setting):
         """Constructor"""
-        super(DoubleEmaStrategy, self).__init__(ctaEngine, setting)
+        super(AtrRsiStrategy, self).__init__(ctaEngine, setting)
 
     #----------------------------------------------------------------------
     def onInit(self):
         """初始化策略（必须由用户继承实现）"""
         self.writeCtaLog(u'%s策略初始化' %self.name)
+    
+        # 初始化RSI入场阈值
+        self.rsiBuy = 50 + self.rsiEntry
+        self.rsiSell = 50 - self.rsiEntry
 
         # 载入历史数据，并采用回放计算的方式初始化策略数值
         initData = self.loadBar(self.initDays)
@@ -104,7 +111,7 @@ class DoubleEmaStrategy(CtaTemplate):
         # 计算K线
         tickMinute = tick.datetime.minute
 
-        if tickMinute != self.barMinute:
+        if tickMinute != self.barMinute:    
             if self.bar:
                 self.onBar(self.bar)
 
@@ -134,6 +141,10 @@ class DoubleEmaStrategy(CtaTemplate):
     #----------------------------------------------------------------------
     def onBar(self, bar):
         """收到Bar推送（必须由用户继承实现）"""
+        # 撤销之前发出的尚未成交的委托（包括限价单和停止单）
+        for orderID in self.orderList:
+            self.cancelOrder(orderID)
+        self.orderList = []
 
         # 保存K线数据
         self.closeArray[0:self.bufferSize-1] = self.closeArray[1:self.bufferSize]
@@ -145,65 +156,67 @@ class DoubleEmaStrategy(CtaTemplate):
         self.lowArray[-1] = bar.low
         
         self.bufferCount += 1
-
-        # EMA 指标技术
-        self.fastEmaValue = round(talib.EMA(self.closeArray, self.fastEmaLength)[-1], 5)
-        self.slowEmaValue = round(talib.EMA(self.closeArray, self.slowEmaLength)[-1], 5)
-
-        self.fastEmaArray[0:self.bufferSize-1] = self.fastEmaArray[1:self.bufferSize]
-        self.fastEmaArray[-1] = self.fastEmaValue
-
-        self.slowEmaArray[0:self.bufferSize-1] = self.slowEmaArray[1:self.bufferSize]
-        self.slowEmaArray[-1] = self.slowEmaValue
-
         if self.bufferCount < self.bufferSize:
-            self.putEvent()
             return
 
-        lastFastEmaArray = self.fastEmaArray[-self.fastEmaLength:]
+        # 计算指标数值
+        self.atrValue = talib.ATR(self.highArray, 
+                                  self.lowArray, 
+                                  self.closeArray,
+                                  self.atrLength)[-1]
+        self.atrArray[0:self.bufferSize-1] = self.atrArray[1:self.bufferSize]
+        self.atrArray[-1] = self.atrValue
 
-        self.fastEmaTradeHigh = lastFastEmaArray.max()
-        self.fastEmaTradeLow = lastFastEmaArray.min()
+        self.atrCount += 1
+        if self.atrCount < self.bufferSize:
+            return
 
+        self.atrMa = talib.MA(self.atrArray, 
+                              self.atrMaLength)[-1]
+        self.rsiValue = talib.RSI(self.closeArray, 
+                                  self.rsiLength)[-1]
+
+        # 判断是否要进行交易
+        
         # 当前无仓位
         if self.pos == 0:
-            # 均线采取EMA,10周期和20周期交叉决定多空
-            if self.fastEmaValue > self.slowEmaValue:
-                # 看多
-                if bar.close > self.fastEmaTradeHigh:
-                    # 多单开仓必须符合价格比前一个EMA10上最高价高
-                    print "Buy: ", bar.close, ", pos: ", self.pos
-                    key = '.'.join([bar.vtSymbol, 'buy'])
-                    self.removePendingOrder(key)
-                    orderID = self.buy(bar.close, self.pre_pos)
-                    self.orderDict[key] = orderID
+            self.intraTradeHigh = bar.high
+            self.intraTradeLow = bar.low
 
+            # ATR数值上穿其移动平均线，说明行情短期内波动加大
+            # 即处于趋势的概率较大，适合CTA开仓
+            if self.atrValue > self.atrMa:
+                # 使用RSI指标的趋势行情时，会在超买超卖区钝化特征，作为开仓信号
+                if self.rsiValue > self.rsiBuy:
+                    # 这里为了保证成交，选择超价5个整指数点下单
+                    self.buy(bar.close+5, 1)
+                    return
 
-            elif self.fastEmaValue < self.slowEmaValue:
-                # 看空
-                if bar.close < self.fastEmaTradeLow:
-                    # 空单开仓必须符合价格比前一个EMA10上最低价低
-                    print "Short: ", bar.close, ", pos: ", self.pos
-                    key = '.'.join([bar.vtSymbol, 'short'])
-                    self.removePendingOrder(key)
-                    orderID = self.short(bar.close, self.pre_pos)
-                    self.orderDict[key] = orderID
+                if self.rsiValue < self.rsiSell:
+                    self.short(bar.close-5, 1)
+                    return
 
         # 持有多头仓位
-        if self.pos > 0 and self.fastEmaValue < self.slowEmaValue and bar.close < self.fastEmaTradeLow:
-            print "cover sell: ", bar.close, ", pos: ", self.pos
-            key = '.'.join([bar.vtSymbol, 'sell'])
-            self.removePendingOrder(key)
-            orderID = self.sell(bar.close, self.pre_pos)
-            self.orderDict[key] = orderID
+        if self.pos == 1:
+            # 计算多头持有期内的最高价，以及重置最低价
+            self.intraTradeHigh = max(self.intraTradeHigh, bar.high)
+            self.intraTradeLow = bar.low
+            # 计算多头移动止损
+            longStop = self.intraTradeHigh * (1-self.trailingPercent/100)
+            # 发出本地止损委托，并且把委托号记录下来，用于后续撤单
+            orderID = self.sell(longStop, 1, stop=True)
+            self.orderList.append(orderID)
+            return
 
         # 持有空头仓位
-        if self.pos < 0 and self.fastEmaValue > self.slowEmaValue and bar.close > self.fastEmaTradeHigh:
-            print "cover buy: ", bar.close, ", pos: ", self.pos
-            key = '.'.join([bar.vtSymbol, 'cover'])
-            self.removePendingOrder(key)
-            orderID = self.cover(bar.close, self.pre_pos)
-            self.orderDict[key] = orderID
+        if self.pos == -1:
+            self.intraTradeLow = min(self.intraTradeLow, bar.low)
+            self.intraTradeHigh = bar.high
+
+            shortStop = self.intraTradeLow * (1+self.trailingPercent/100)
+            orderID = self.cover(shortStop, 1, stop=True)
+            self.orderList.append(orderID)
+            return
 
         # 发出状态更新事件
         self.putEvent()
@@ -211,24 +224,11 @@ class DoubleEmaStrategy(CtaTemplate):
     #----------------------------------------------------------------------
     def onOrder(self, order):
         """收到委托变化推送（必须由用户继承实现）"""
-        self.putEvent()
         pass
 
     #----------------------------------------------------------------------
     def onTrade(self, trade):
-        """收到成交推送"""
-        for key, value in self.orderDict.items():
-            if value == trade.orderID:
-                self.orderDict.pop(key)
-        self.putEvent()
         pass
-
-    def removePendingOrder(self, key):
-        if key in self.orderDict:
-            orderId = self.orderDict.pop(key)
-            if orderId:
-                print "removePendingOrder: ", key, orderId
-                self.cancelOrder(orderId)
 
 
 if __name__ == '__main__':
@@ -244,18 +244,18 @@ if __name__ == '__main__':
     engine.setBacktestingMode(engine.BAR_MODE)
 
     # 设置回测用的数据起始日期
-    engine.setStartDate('20160301')
+    engine.setStartDate('20120101')
     
     # 载入历史数据到引擎中
     engine.loadHistoryData(MINUTE_DB_NAME, 'IF0000')
     
     # 设置产品相关参数
-    #engine.setSlippage(0.2)     # 股指1跳
-    #engine.setRate(0.3/10000)   # 万0.3
-    #engine.setSize(300)         # 股指合约大小
+    engine.setSlippage(0.2)     # 股指1跳
+    engine.setRate(0.3/10000)   # 万0.3
+    engine.setSize(300)         # 股指合约大小    
     
     # 在引擎中创建策略对象
-    engine.initStrategy(DoubleEmaStrategy, {})
+    engine.initStrategy(AtrRsiStrategy, {})
     
     # 开始跑回测
     engine.runBacktesting()
